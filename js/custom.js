225301e280cb5677af017bab02589df2a7818863
Nikeshsam <31123343+Nikeshsam@users.noreply.github.com>
2021-06-24 13:26:14 +0530

    Mega Menu Added
    Mega Menu Added

--- js/custom.js
+++ js/custom.js
@@ -143,6 +143,18 @@ $(".View").click(function () {
 });
 
 
+$(".dropdown").hover(
+	function () {
+		$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+		$(this).toggleClass('open');
+	},
+	function () {
+		$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
+		$(this).toggleClass('open');
+	}
+);
+
+
 // Main/Product image slider for product page
 $('.ProductDescLeft .main-img-slider').slick({
 	slidesToShow: 1,
