4f7c9c78bb4579b336d7a9365e215224368e8485
Nikeshsam <31123343+Nikeshsam@users.noreply.github.com>
2021-07-01 12:06:28 +0530

    New Updates

--- js/custom.js
+++ js/custom.js
@@ -145,11 +145,11 @@ $(".View").click(function () {
 
 $(".dropdown").hover(
 	function () {
-		$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+		$('.mega-dropdown-menu.dropdown-menu', this).not('.in .mega-dropdown-menu.dropdown-menu').stop(true, true).slideDown("400");
 		$(this).toggleClass('open');
 	},
 	function () {
-		$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
+		$('.mega-dropdown-menu.dropdown-menu', this).not('.in .mega-dropdown-menu.dropdown-menu').stop(true, true).slideUp("400");
 		$(this).toggleClass('open');
 	}
 );
